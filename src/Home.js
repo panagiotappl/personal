1ee050456fc3affd9d2692f8f3361bf2b3458ea3
Panagiota Poulopoulou <panagiotappl@gmail.com>
2019-09-21 15:35:44 +0200

    Add navigation bar, Quicksand font and favicon

--- src/Home.js
+++ src/Home.js
@@ -7,7 +7,10 @@ export default function Home() {
     <div className="home">
       <header className="home-title">
         <Transition transitionOnMount duration={2000}>
-            <div>Welcome to my personal page Annoula</div>
+            <div className="navigation-button">Projects</div>
+        </Transition>
+        <Transition transitionOnMount duration={2000}>
+            <div className="navigation-button">Travels</div>
         </Transition>
       </header>
     </div>
